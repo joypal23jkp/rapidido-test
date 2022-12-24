336f9af2b007b5fa556217f10ab412ca7b3da2d1
Joy Kumar Pal <joypal.23.jkp@gmail.com>
2022-12-25 02:24:41 +0600

    :sparkless: feat(merchant-auth): updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,12 +5,12 @@ import Github from "next-auth/providers/github";
 const config = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID || '333967899513-9pl4bi3tb7i8esp1hbaf2l8gklrl351d.apps.googleusercontent.com',
-            clientSecret: process.env.GOOGLE_SECRET || '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',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
         }),
         Github({
-            clientId: process.env.GITHUB_ID || '3f055dde5b0afbe66c39',
-            clientSecret: process.env.GITHUB_SECRET || '5432f95e691224b88d413a2b5332e8947c04342d',
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
